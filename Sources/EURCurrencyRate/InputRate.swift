8fbb546810c3ddecf79d4c98354e2c94e27a09fe
calebakeyes <65526474+calebakeyes@users.noreply.github.com>
2021-01-31 21:19:40 -0500

    Add files via upload

---
+++ Sources/EURCurrencyRate/InputRate.swift
@@ -0,0 +1,13 @@
+//
+//  File.swift
+//  
+//
+//  Created by Caleb Keyes on 1/29/21.
+//
+
+import Foundation
+
+struct InputRate: Codable {
+    let amountOfMoney: Double
+    let currencyType: String?
+}
